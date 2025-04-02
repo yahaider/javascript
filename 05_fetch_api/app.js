775358dc9d440ca9569f817223ac964f0ecba39c
yahaider <yahaiderekarrar@gmail.com>
2025-04-02 12:01:51 +0500

    Simple fetch API - with jsonplacehoder endpoint

---
+++ 05_fetch_api/app.js
@@ -0,0 +1,22 @@
+const url = "https://jsonplaceholder.typicode.com/users";
+fetch(url)
+  .then(res => {
+    if (!res.ok) throw new Error('Failed to fetch data...!');
+    return res.json();
+  })
+  .then(res => console.log(res))
+  .catch(err => console.error('Error:', err.message));
+
+
+// const url = "https://jsonplaceholder.typicode.com/users";
+
+// fetch(url)
+// .then((res)=>{
+//     if (!res.ok) throw new Error('Request failed!!!'); return res.json();
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
