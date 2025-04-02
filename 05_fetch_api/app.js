2bb3a3998f8f5a631576d3b2f16d6c37ec227057
yahaider <yahaiderekarrar@gmail.com>
2025-04-02 15:47:04 +0500

    Added few examples to fetch Image using fetch() api

--- 05_fetch_api/app.js
+++ 05_fetch_api/app.js
@@ -1,3 +1,53 @@
+// const url = 'flowers.jpg';
+// const myRequest = new Request(url);
+// const image = document.querySelector('img');
+
+// fetch(myRequest).then((res)=>{
+//     if(!res.ok) throw new Error('something went wrong!!!');
+//     return res.blob();
+// }).then((data)=>{
+//     const objURL = URL.createObjectURL(data)
+//     image.src = objURL;
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+// async function GetImage(){
+//     const response = await fetch('flowers.jpg');
+//     const blob = await response.blob();
+//     const image = document.querySelector("img");
+//     const objectURL = URL.createObjectURL(blob);
+//     image.src = objectURL;
+// }
+
+// GetImage().catch(error => {
+//     console.error(error);
+// });
+
+// console.log("about to fetch a flower");
+
+// const image = document.querySelector("img");
+
+// const url = "flowers.jpg";
+
+// async function setImage() {
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+//     const blob = await response.blob();
+//     const objectURL = URL.createObjectURL(blob);
+//     image.src = objectURL;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+
+// setImage();
+
+
 // const url = "https://jsonplaceholder.typicode.com/users";
 // fetch(url)
 //   .then(res => {
